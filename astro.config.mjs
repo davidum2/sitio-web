b237c41ed1497cc774857cdc7f8831dd62cee29c
davidum2 <davidum215@gmail.com>
2025-08-16 07:40:35 -0600

    verificacion antes de salir cuu

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-
-  site: 'https://davidum2.github.io',
-  base: '/sitio-web',
+  site: 'https://senderozen.com',
 });
