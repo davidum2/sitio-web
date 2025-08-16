64177cd2f86edb54508d6b918a1e9119f899e213
davidum2 <davidum215@gmail.com>
2025-08-15 21:10:24 -0600

    despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,9 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+
+  site: 'https://davidum2.github.io',
+  // Reemplaza <el-nombre-de-tu-repo> con el nombre de tu repositorio
+  base: '/sitio-web',
 });
